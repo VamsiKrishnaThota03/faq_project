a12676e8a3bcb10643588e24a7db329560610a74
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 22:52:56 +0530

    feat: Add automated translation using Google Translate API

--- faq/models.py
+++ faq/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
+
+translator = Translator()
+
+def translate_text(text, dest_lang):
+    try:
+        return translator.translate(text, dest=dest_lang).text
+    except:
+        return text
+    
 
 class FAQ(models.Model):
     question = models.TextField()
@@ -17,3 +27,14 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+    
+    def save(self, *args, **kwargs):
+        if not self.question_hi:
+            self.question_hi = translate_text(self.question, 'hi')
+        if not self.question_bn:
+            self.question_bn = translate_text(self.question, 'bn')
+        if not self.answer_hi:
+            self.answer_hi = translate_text(self.answer, 'hi')
+        if not self.answer_bn:
+            self.answer_bn = translate_text(self.answer, 'bn')
+        super().save(*args, **kwargs)
