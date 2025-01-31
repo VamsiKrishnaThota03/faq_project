392f8511388b0c05e9ed1991e3a05566364ac5da
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 21:17:49 +0530

    feat: Add FAQ model with multi-language support

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    question_hi = models.TextField(blank=True, null=True)  # Hindi translation
+    question_bn = models.TextField(blank=True, null=True)  # Bengali translation
+    answer_hi = models.TextField(blank=True, null=True)
+    answer_bn = models.TextField(blank=True, null=True)
+
+    def get_translated_question(self, lang='en'):
+        return getattr(self, f'question_{lang}', self.question)
+
+    def get_translated_answer(self, lang='en'):
+        return getattr(self, f'answer_{lang}', self.answer)
+
+    def __str__(self):
+        return self.question
