6a7f7a2d66ee2a19b68ed03cf1466996386503d3
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 22:43:28 +0530

    feat: Add Redis caching for FAQ API

--- faq/views.py
+++ faq/views.py
@@ -2,13 +2,19 @@ from rest_framework import generics
 from .models import FAQ
 from .serializers import FAQSerializer
 
+from django.core.cache import cache
+
 class FAQListView(generics.ListAPIView):
     serializer_class = FAQSerializer
 
     def get_queryset(self):
         lang = self.request.query_params.get('lang', 'en')
-        queryset = FAQ.objects.all()
-        for faq in queryset:
-            faq.question = faq.get_translated_question(lang)
-            faq.answer = faq.get_translated_answer(lang)
+        cache_key = f'faqs_{lang}'
+        queryset = cache.get(cache_key)
+        if not queryset:
+            queryset = FAQ.objects.all()
+            for faq in queryset:
+                faq.question = faq.get_translated_question(lang)
+                faq.answer = faq.get_translated_answer(lang)
+            cache.set(cache_key, queryset, timeout=60 * 15)  # Cache for 15 minutes
         return queryset
