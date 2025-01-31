e0545aa8ef46d1b3be4bd72b279af831ef3763f5
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 22:37:45 +0530

    feat: Add FAQ model with multilingual support

--- faq/models.py
+++ faq/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class FAQ(models.Model):
-    question = models.CharField(max_length=255)
-    answer = models.TextField()
+    question = models.TextField()
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True, null=True)  # Hindi translation
+    question_bn = models.TextField(blank=True, null=True)  # Bengali translation
+    answer_hi = RichTextField(blank=True, null=True)  # Hindi translation
+    answer_bn = RichTextField(blank=True, null=True)  # Bengali translation
+
+    def get_translated_question(self, lang):
+        return getattr(self, f'question_{lang}', self.question)
+
+    def get_translated_answer(self, lang):
+        return getattr(self, f'answer_{lang}', self.answer)
 
     def __str__(self):
         return self.question
