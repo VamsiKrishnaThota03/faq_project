f98a714db44a1f34242ecac8df7d60b289efffc3
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 23:45:07 +0530

    feat: Add FAQ API with language support

--- faq/urls.py
+++ faq/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import FAQListView
 
 urlpatterns = [
     path('faqs/', FAQListView.as_view(), name='faq-list'),
+    path('api/', include('faq.urls')),
 ]
