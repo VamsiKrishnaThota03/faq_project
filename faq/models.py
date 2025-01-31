92c72bb8b03ac8759a5e9972d67936af31aec07b
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-01-31 21:20:11 +0530

    feat: Integrate django-ckeditor for WYSIWYG support

--- faq/models.py
+++ faq/models.py
@@ -1,18 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class FAQ(models.Model):
     question = models.TextField()
-    answer = models.TextField()
-    question_hi = models.TextField(blank=True, null=True)  # Hindi translation
-    question_bn = models.TextField(blank=True, null=True)  # Bengali translation
-    answer_hi = models.TextField(blank=True, null=True)
-    answer_bn = models.TextField(blank=True, null=True)
-
-    def get_translated_question(self, lang='en'):
-        return getattr(self, f'question_{lang}', self.question)
-
-    def get_translated_answer(self, lang='en'):
-        return getattr(self, f'answer_{lang}', self.answer)
-
-    def __str__(self):
-        return self.question
+    answer = RichTextField()  # WYSIWYG editor
+    question_hi = models.TextField(blank=True, null=True)
+    question_bn = models.TextField(blank=True, null=True)
+    answer_hi = RichTextField(blank=True, null=True)
+    answer_bn = RichTextField(blank=True, null=True)
