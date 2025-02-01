306f334ad26f3f1d270a885d2285a626c9fc3edb
VamsiKrishnaThota03 <vamsikrishnathota483@gmail.com>
2025-02-01 22:24:15 +0530

    fix: Add root URL route and home view

--- faq_project/urls.py
+++ faq_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from faq.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/', include('faq.urls')),
-    # path('', include('faq.urls')),
+    path('', home, name='home'),
 ]
